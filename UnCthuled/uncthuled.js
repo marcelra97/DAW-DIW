0e215fc8f9f345c4acc623a49bb9ee47c185eea1
Marcel Ramirez Novillo <marcelramireznovillo@gmail.com>
2019-10-14 16:53:45 +0200

    + Haciendo el juego

---
+++ UnCthuled/uncthuled.js
@@ -0,0 +1,23 @@
+var arrayMapa = new Array();
+
+function cargarMapa() {
+
+    var nodoPadre = document.get;
+
+    for (let x = 0; x < 12; x++) {
+
+        for (let y = 0; y < 20; y++) {
+
+            document.createElement("div");
+        }
+
+    }
+}
+var document.
+
+
+window.onload = function() {
+
+
+
+};
