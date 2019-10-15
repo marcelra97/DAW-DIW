c1209fc5e1e9c996aee680a524d4016b32077db1
Marcel Ramirez Novillo <marcelramireznovillo@gmail.com>
2019-10-15 20:24:41 +0200

    +He creado el mapa y me aparece el personaje

--- UnCthuled/uncthuled.js
+++ UnCthuled/uncthuled.js
@@ -1,23 +1,60 @@
 var arrayMapa = new Array();
 
+window.onload = function() {
+
+    cargarMapa()
+
+};
+
 function cargarMapa() {
 
-    var nodoPadre = document.get;
 
-    for (let x = 0; x < 12; x++) {
+    //var padre = document.getElementById("mapa");
 
-        for (let y = 0; y < 20; y++) {
+    let arrayMapa = [
+        [7, 7, 7, 7, 7, 7, 7, 7, 2, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
 
-            document.createElement("div");
+    for (let x = 0; x < arrayMapa.length; x++) {
+
+        for (let y = 0; y < arrayMapa[0].length; y++) {
+
+            var hijo = document.createElement("div");
+
+            if (arrayMapa[x][y] == 0) {
+
+                hijo.classList.add("camino");
+
+            }
+
+            if (arrayMapa[x][y] == 1) {
+
+                hijo.classList.add("pilar");
+            }
+
+            if (arrayMapa[x][y] == 2) {
+
+                hijo.classList.add("personaje");
+            }
+
+            document.getElementById("mapa").appendChild(hijo);
         }
 
     }
+
+
+
 }
-var document.
-
-
-window.onload = function() {
-
-
-
-};
