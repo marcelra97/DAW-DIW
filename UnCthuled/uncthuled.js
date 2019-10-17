8eb51afc0729aef0da9791dab2e2a1761ac133a9
Marcel Ramirez Novillo <marcelramireznovillo@gmail.com>
2019-10-17 17:57:53 +0200

    + He conseguido agrupar los pilares en varias clases

--- UnCthuled/uncthuled.js
+++ UnCthuled/uncthuled.js
@@ -37,6 +37,12 @@ arrayMapa = [
 var x = 0;
 var y = 8;
 
+var pilarPar = 1;
+var pilarImpar = 1;
+
+var contadorPar = 0;
+var contadorImpar = 0;
+
 function cargarMapa() {
 
     for (let filas = 0; filas < arrayMapa.length; filas++) {
@@ -54,6 +60,41 @@ function cargarMapa() {
             if (arrayMapa[filas][columnas] == 1) {
 
                 hijo.classList.add("pilar");
+
+                /*Me he creado una segunda clase para pilar, para asi poder trabajar con los grupos de pilares*/
+
+                //Primero compruebo las partes pares y le cambio los tres divs de arriba al pilar y les añado una clase
+                if (filas % 2 == 0 && contadorPar < 3) {
+
+                    //si lo es añado la clase pilar
+                    hijo.classList.add("pilar" + pilarPar);
+
+                    //y le sumo al contador par
+                    contadorPar++;
+
+                    //Compruebo las partes impares y le cambio los tres divs de abajo al pilar y les añado una clase    
+                } else if (filas % 2 != 0 && contadorImpar < 3) {
+
+                    hijo.classList.add("pilar" + pilarImpar);
+
+                    contadorImpar++;
+
+                }
+
+                //Cada tres pilares que cambio, reinicio el contador y al siguiente grupo le doy otro numero de pilar
+                if (contadorPar == 3) {
+
+                    contadorPar = 0;
+                    pilarPar++;
+                }
+
+                //Cada tres pilares que cambio, reinicio el contador y al siguiente grupo le doy otro numero de pilar
+                if (contadorImpar == 3) {
+
+                    contadorImpar = 0;
+                    pilarImpar++;
+                }
+
             }
 
             if (arrayMapa[filas][columnas] == 2) {
@@ -75,8 +116,9 @@ function cargarMapa() {
             arrayMapa[filas][columnas] = hijo;
         }
 
+
     }
-    //console.table(arrayMapa);
+    console.table(arrayMapa);
 
 }
 
@@ -121,9 +163,9 @@ function movimientoPersonaje(teclado) {
 function moverAbajo() {
 
     //Cuando llegue al fondo el personaje no puede moverse mas para abajo si el siguiente es la fila 14
-    if(x+1 != 14){
+    if (x + 1 != 14) {
 
-    /*Si lo que hay abajo del personaje contiene un pilar no puede avanzar hacia esa direccion */
+        /*Si lo que hay abajo del personaje contiene un pilar no puede avanzar hacia esa direccion */
         if (!arrayMapa[x + 1][y].classList.contains("pilar")) {
 
             arrayMapa[x][y].classList.remove("personajeAbajo");
@@ -141,9 +183,9 @@ function moverAbajo() {
 /*Cuando me muevo hacia arriba me borro en la posicion anterior el personaje y le añado un camino*/
 function moverArriba() {
 
-    if(x-1 != -1){
+    if (x - 1 != -1) {
 
-        if (!arrayMapa[x - 1][y].classList.contains("pilar") && !arrayMapa[x-1][y].classList.contains("muro") ) {
+        if (!arrayMapa[x - 1][y].classList.contains("pilar") && !arrayMapa[x - 1][y].classList.contains("muro")) {
 
             arrayMapa[x][y].classList.remove("personajeAbajo");
             arrayMapa[x][y].classList.remove("personajeDerecha");
@@ -159,10 +201,10 @@ function moverArriba() {
 
 /*Cuando me muevo hacia derecha me borro en la posicion anterior el personaje y le añado un camino*/
 function moverDerecha() {
-    
-   if(y+1 != 21){ 
 
-        if (!arrayMapa[x][y + 1].classList.contains("pilar") && !arrayMapa[x][y + 1].classList.contains("muro") ) {
+    if (y + 1 != 21) {
+
+        if (!arrayMapa[x][y + 1].classList.contains("pilar") && !arrayMapa[x][y + 1].classList.contains("muro")) {
 
             arrayMapa[x][y].classList.remove("personajeAbajo");
             arrayMapa[x][y].classList.remove("personajeDerecha");
@@ -179,9 +221,9 @@ function moverDerecha() {
 /*Cuando me muevo hacia izquierda me borro en la posicion anterior el personaje y le añado un camino*/
 function moverIzquierda() {
 
-    if(y-1 !=-1){  
+    if (y - 1 != -1) {
 
-        if (!arrayMapa[x][y - 1].classList.contains("pilar") && !arrayMapa[x][y - 1].classList.contains("muro") ) {
+        if (!arrayMapa[x][y - 1].classList.contains("pilar") && !arrayMapa[x][y - 1].classList.contains("muro")) {
 
             arrayMapa[x][y].classList.remove("personajeAbajo");
             arrayMapa[x][y].classList.remove("personajeDerecha");
