2a943b050b8c1aa19551ad5e4249d299d6118bcd
Marcel Ramirez Novillo <marcelramireznovillo@gmail.com>
2019-10-30 20:41:51 +0100

    +MEjorando la interfaz

--- UD02/Practicas/tarea08/js/Tarea_08.js
+++ UD02/Practicas/tarea08/js/Tarea_08.js
@@ -1,17 +1,42 @@
-
 window.addEventListener('keydown', function(evento) {
-    
+
     //me guardo la key
     var key = evento.keyCode;
-    
+
     pulsarTecla(key);
 
 }, false);
 
-function pulsarTecla(evento){
+function pulsarTecla(evento) {
 
     //me coge los divs del boton
-    var boton = document.getElementById(evento);
+    let boton = document.getElementById(evento);
 
     boton.classList.add("transicion");
+
+    if (evento == "81") {
+
+        playMusic("braumQ");
+
+    } else if (evento == "87") {
+
+        playMusic("braumW");
+
+    } else if (evento == "69") {
+
+        playMusic("braumE");
+
+    } else if (event == "82") {
+
+        playMusic("braumR");
+
+    }
+
+
+}
+
+function playMusic(tecla) {
+
+    let audio = document.querySelector(`audio [data-key=${tecla.keyCode}]`);
+    audio.play();
 }
