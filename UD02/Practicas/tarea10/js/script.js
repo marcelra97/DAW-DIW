9265ff476331ca677b9b6a2e491fbeeaeceb3b65
Marcel Ramirez Novillo <marcelramireznovillo@gmail.com>
2019-11-06 20:32:03 +0100

    + Acabando cosas

--- UD02/Practicas/tarea10/js/script.js
+++ UD02/Practicas/tarea10/js/script.js
@@ -1,18 +1,15 @@
-window.onload= function(){
-    //llamo a la funcion
-    pulsarBoton();
-}
+window.onload = function() { document.querySelector('button').addEventListener('click', function transicionImagen() { document.querySelectorAll(".caja").forEach(caja => caja.classList.toggle("movimiento")); }); }
 
-function pulsarBoton(){
-    //hago un querySelector del boton y le añado un eventListener que llama a la transicion
-    document.querySelector("button").addEventListener("click", transicionImagen);
+// function pulsarBoton(){
+//     //hago un querySelector del boton y le añado un eventListener que llama a la transicion
+//     document.querySelector("button").addEventListener("click", transicionImagen);
 
-}
+// }
 
-function transicionImagen(){
-    //me guardo en un array todos los elementos que tenga la clase caja
-    const keys = document.querySelectorAll(".caja")
-    
-    //y hago un foreach para recorer cada una y les añado o les quito las clase movimiento
-    keys.forEach(caja => caja.classList.toggle("movimiento"));
-}
+// function transicionImagen(){
+//     //me guardo en un array todos los elementos que tenga la clase caja
+//     const keys = document.querySelectorAll(".caja")
+
+//     //y hago un foreach para recorer cada una y les añado o les quito las clase movimiento
+//     keys.forEach(caja => caja.classList.toggle("movimiento"));
+// }
--- UD02/Practicas/tarea12/js/script.js
+++ UD02/Practicas/tarea12/js/script.js
@@ -1,25 +1,45 @@
+function init() {
 
-function init(){
+    document.querySelector('button').addEventListener('click', crearCaja);
 
-    document.querySelector("button").addEventListener("click", crearCaja);
-   
 }
 
-function crearCaja(){
+function crearCaja() {
 
-    const nodoPadre = document.querySelector("container");
-    const box = document.createElement("box");
+    const nodoPadre = document.querySelector('container');
+    const box = document.createElement('box');
 
     nodoPadre.appendChild(box);
 
-    box.addEventListener("click", desplazarAdelante);
+    box.addEventListener('click', desplazarCaja);
 }
 
-function desplazarAdelante(e){
+function desplazarCaja(e) {
 
-   let caja = e.target;
+    let caja = e.target;
 
-   caja.classList.toggle("evoluciona");
+    caja.classList.add('evoluciona');
+
+
+    caja.addEventListener('click', vuelveCaja);
+
 }
 
-window.onload=init;
+function vuelveCaja(e) {
+
+    e.taget.classList.remove('evoluciona');
+
+    e.target.classList.add('desevoluciona');
+
+    caja.addEventListener('click', apareceSedefkar);
+
+}
+
+function apareceSedefkar(e) {
+
+
+    e.target.classList.add('ultimate');
+
+}
+
+window.onload = init;
