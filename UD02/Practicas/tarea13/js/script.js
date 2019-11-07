b58aefad45c103fe2f75d0a192e53a06614077c8
Marcel Ramirez Novillo <44468996+marcelra97@users.noreply.github.com>
2019-11-07 13:01:29 +0100

    + Creada la taera 13

---
+++ UD02/Practicas/tarea13/js/script.js
@@ -0,0 +1,53 @@
+function init() {
+
+    document.querySelector('button').addEventListener('click', crearCaja);
+    
+    document.querySelector('.rodar').addEventListener('click', efectoRodar);
+
+    document.addEventListener('.rebotar').addEventListener('click', efectoRebote);
+    
+}
+
+function crearCaja() {
+
+    const nodoPadre = document.querySelector('container');
+    const box = document.createElement('box');
+
+    nodoPadre.appendChild(box);
+
+    box.addEventListener('click', desplazarCaja);
+}
+
+function desplazarCaja(e) {
+    
+    let caja = e.target;
+
+    if(!caja.classList.contains('evoluciona') && !caja.classList.contains('desevoluciona') && !caja.classList.contains('ultimate') ){
+        
+        caja.classList.add("evoluciona");
+       
+    }else if(!caja.classList.contains('desevoluciona') && caja.classList.contains('evoluciona')){
+
+        caja.classList.remove("evoluciona");
+        caja.classList.add("desevoluciona");
+
+    }else if(caja.classList.contains('desevoluciona')){
+
+        caja.classList.remove('desevoluciona');
+        caja.classList.add('ultimate');
+        caja.classList.add('ultimate:hover');
+    }
+
+
+}
+
+function efectoRodar(){
+
+}
+
+function efectoRebote(){
+
+
+}
+
+window.onload = init;
