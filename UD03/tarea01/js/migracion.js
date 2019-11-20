891a86f5d1d0b7eec16cd4fee774382418b2fb99
Marcel Ramirez Novillo <44468996+marcelra97@users.noreply.github.com>
2019-11-20 13:50:18 +0100

    + ya hace las transiciones, falta que las haga en orden

--- UD03/tarea01/js/migracion.js
+++ UD03/tarea01/js/migracion.js
@@ -7,21 +7,41 @@ email : <berlanas_ang@gva.es>
 
 */
 
-/*
 
-FUNCIONES PERDIDAS
-^(;,;)^
 
-*/
+// function avanzaBarra(progreso){
+   
+//     let value = progreso.getAttribute("value");
+    
+//     console.log(value);
+
+// }
+
+function hacerProcesos(progreso) {
+    
+   progreso.classList.remove("oculto");
+   progreso.classList.add("estabaEscondido");
+
+}
 
 function startMigration(){
 
-    // Fragmentos perdidos
-    // ^(;,;)^
+    let step = document.querySelectorAll('[data-step]');
+    
+    step.forEach(progreso => {
+
+
+        hacerProcesos(progreso);
+    });
+
 }
 
 function init(){
     console.info(" * Init envirnoment ");
+
+    let etiquetas = document.querySelectorAll('[data-step]')
+
+    etiquetas.forEach(etiqueta => etiqueta.classList.add("oculto"));
 
     // Set click function on button
     document.querySelector("button").addEventListener("click",startMigration);
