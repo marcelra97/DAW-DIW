39cb321c409b5ab4c137ee4757397176ae0365de
Marcel Ramirez Novillo <marcelramireznovillo@gmail.com>
2019-11-21 17:33:25 +0100

    +Hechas las transiciones falta hacer que la barra de progreso vaya

--- UD03/tarea01/js/migracion.js
+++ UD03/tarea01/js/migracion.js
@@ -17,22 +17,27 @@ email : <berlanas_ang@gva.es>
 
 // }
 
-function hacerProcesos(progreso) {
-
-
-    progreso.classList.add("estabaEscondido");
-
-
-
-}
+let listaStep;
+let contador = 0;
 
 function startMigration() {
 
-    let step = document.querySelectorAll('[data-step]');
+    if (contador < listaStep.length) {
 
-    for (let i = 0; i < step.length; i++) {
+        listaStep[contador].classList.add("estabaEscondido");
 
-        hacerProcesos[i];
+        //en este if hacer la barra de progreso
+        if (listaStep[contador].localName == "progress") {
+
+            //me coge el valor de la barra
+            //haz un .value para poder mover el value
+            let barra = listaStep[contador].getAttribute("value");
+
+
+        }
+
+        listaStep[contador].addEventListener('transitionend', startMigration);
+        contador++;
     }
 
 
@@ -41,6 +46,8 @@ function startMigration() {
 
 function init() {
     console.info(" * Init envirnoment ");
+
+    listaStep = document.querySelectorAll('[data-step]');
 
     // Set click function on button
     document.querySelector("button").addEventListener("click", startMigration);
