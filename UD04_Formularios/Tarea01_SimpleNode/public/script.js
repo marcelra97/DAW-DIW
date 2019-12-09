0f1c357493cb07ef3bde1ab020751543f1fcd57e
Marcel Ramirez Novillo <44468996+marcelra97@users.noreply.github.com>
2019-12-09 12:20:56 +0100

    Casi va bien el buscador y tambien falta utilizar el sass

--- UD04_Formularios/Tarea01_SimpleNode/public/script.js
+++ UD04_Formularios/Tarea01_SimpleNode/public/script.js
@@ -1,20 +1,52 @@
+
+function filtro(listaMonumentos) {
+  
+  //aqui me cojo la palabra
+  let palabra = document.querySelector('[name="palabra"]').value; 
+
+  //con el array de los monumentos que contienen la palabra busco el nombre de esta y solamente cojo las que empiecen por la palabra
+   let filtroMonumentos = listaMonumentos.properties.nombre.startsWith(palabra.toUpperCase());
+
+   //y las devuelvo
+   return filtroMonumentos;
+}
+
 function busqueda (){
 
-    let lista = document.createElement('ul');
+  let listado = document.createElement("ul");
 
+  //no se como funciona fetch, solo se que hace cosas, preguntar a Angel como va esto o a mi lider natural Javi
     fetch(conexion)
   .then(response => response.json())
   .then(function(myJson) {
-    console.log(myJson);
-    console.log(myJson.features);
+    
+    //Aqui me da un array de todos los monumentos y utilizo el metodo filter del array 
+    nombreMonumentos = myJson.features.filter(filtro);
+    
+    //recorro el array filtrado para poder mostrar todos los monumentos
+    nombreMonumentos.forEach(monumentos => {
+
+      let lista = document.createElement("li");
+
+      lista.innerHTML = monumentos.properties.nombre;
+
+      listado.appendChild(lista);
+
+    });
+  
+    document.querySelector('#resultado').appendChild(listado);
 
 });
+
 
 }
 
 function init() {
-    document.querySelector('[name="busqueda"]').addEventListener('keyup', busqueda);
+
+   document.querySelector('[name="busqueda"]').addEventListener('click', busqueda);
+   
 }
 
-const conexion =  "http://mapas.valencia.es/lanzadera/opendata/Valenbisi/JSON";
+let nombreMonumentos;
+const conexion =  "http://mapas.valencia.es/lanzadera/opendata/Monumentos-turisticos/JSON";
 window.onload = init;
